a70bd342773ca162f3ffa95132cd076fc1ea5a2c
venkatesh30-sm <venkateshsmtester@gmail.com>
2020-04-12 13:23:22 +0530

    code added

---
+++ src/org/cts/CourseStructure.java
@@ -0,0 +1,22 @@
+package org.cts;
+
+public class CourseStructure {
+
+	
+	public void courseDuration() {
+	
+		System.out.println("course duration is 2 months");
+
+	}
+	
+	public static void main(String[] args) {
+		
+		//Object creation
+		CourseStructure c=new CourseStructure();
+		
+		//To call the method
+		c.courseDuration();
+		
+	}
+
+}
