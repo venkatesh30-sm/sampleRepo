3413c81e9fc79443f66deb1d4f21a48307d7242e
venkatesh30-sm <venkateshsmtester@gmail.com>
2020-04-14 12:27:07 +0530

    arun done with his work

--- src/org/cts/CourseStructure.java
+++ src/org/cts/CourseStructure.java
@@ -1,15 +1,23 @@
 package org.cts;
 
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.event.KeyEvent;
+
 public class CourseStructure {
 
 	
-	public void courseDuration() {
+	public void courseDuration() throws AWTException {
 	
 		System.out.println("course duration is 2 months");
+		
+		Robot r=new Robot();
+		r.keyPress(KeyEvent.VK_ENTER);
+		r.keyRelease(KeyEvent.VK_ENTER);
 
 	}
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws AWTException {
 		
 		//Object creation
 		CourseStructure c=new CourseStructure();
